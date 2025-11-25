1bcd0dba9d70b95a0f6c9c93c53ad841da523485
Thomas Cazzolato <2444121@cegeplimoilou.ca>
2025-11-25 17:27:26 -0500

    test sur la branche Thomas_1 uart Tx fini je crois

--- Logiciel/STM/STM/Core/Inc/UART_Com.h
+++ Logiciel/STM/STM/Core/Inc/UART_Com.h
@@ -1,6 +1,9 @@
 // ************** INCLUDES ************** //
 #include "main.h"
 
+// ************** EXTERN **************** //
+extern UART_HandleTypeDef huart1;
+
 // ************** DEFINES *************** //
 #ifndef INC_UART_Com_H_
 #define INC_UART_Com_H_
