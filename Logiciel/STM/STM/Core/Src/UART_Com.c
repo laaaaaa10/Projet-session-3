3930e2b9f7b05cc7853a890a9bed9c40123f2fe1
Javier Bolduc <2436653@cegeplimoilou.ca>
2025-11-26 09:30:09 -0500

    fixed a coupple of things, now ready to write 0 to the 6 motors

--- Logiciel/STM/STM/Core/Src/UART_Com.c
+++ Logiciel/STM/STM/Core/Src/UART_Com.c
@@ -2,21 +2,21 @@
 #include "UART_Com.h"
 
 // ************** FUNCTION ************** //
-void UART_Envoit(uint8_t M_0, uint8_t M_1, uint8_t M_2, uint8_t M_3, uint8_t M_4)
+void UART_Send(uint8_t M_0, uint8_t M_1, uint8_t M_2, uint8_t M_3, uint8_t M_4)
 {
     uint8_t CheckSum;
-    UART_STM('G');
-    UART_STM('O');
-    UART_STM(M_0);
-    UART_STM(M_1);
-    UART_STM(M_2);
-    UART_STM(M_3);
-    UART_STM(M_4);
+    UART_Com('G');
+    UART_Com('O');
+    UART_Com(M_0);
+    UART_Com(M_1);
+    UART_Com(M_2);
+    UART_Com(M_3);
+    UART_Com(M_4);
     CheckSum = ('G' + 'O' + M_0 + M_1 + M_2 + M_3 + M_4);
-    UART_STM(CheckSum);
+    UART_Com(CheckSum);
 }
 
-void UART_STM(uint8_t V_TX)
+void UART_Com(uint8_t V_TX)
 {
     HAL_UART_Transmit(&huart1, &V_TX, 1, 1);
 }
