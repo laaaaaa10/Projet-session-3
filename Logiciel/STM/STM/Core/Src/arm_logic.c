9b6d64dee2849761784d5f3531d88043246f5b66
laaaaaa10 <laaaaaaa104@gmail.com>
2025-11-27 22:54:08 -0500

    fixed the arm logic comments

--- Logiciel/STM/STM/Core/Src/arm_logic.c
+++ Logiciel/STM/STM/Core/Src/arm_logic.c
@@ -10,10 +10,10 @@ Z
 // *the robot base pivot is at 0,0,10 
 // *the angles of a pivot point are relative to the previous segment*
 // *the angle is the total ratio 360 degrees = to 5 rotations of the motor*
-// minmax angle for motor 0 (base rotation) = inifinite
-// minmax angle for motor 1 (first pivot point) = 45..200 degrees
-// minmax angle for motor 2 (second pivot point) = 45..300 degrees
-// minmax angle for motor 3 (wrist) = 45..300 degrees
+// minmax angle for motor 0 (base rotation) = -169..169
+// minmax angle for motor 1 (first pivot point) = 45..180 degrees
+// minmax angle for motor 2 (second pivot point) = 300..60 degrees
+// minmax angle for motor 3 (wrist) = 300..60 degrees
 // minmax angle for motor 4 (hand open/close) = 0(close)..125(open) degrees 
 //
 // note that Z is not going to work, it will constantly be at 5cm from the buildplate
@@ -35,7 +35,7 @@ float L2 = 25.5;
 
 //************************* SETUP MAIN PROGRAM *******************************
 // the logic of the 2 pivot points and hand control is in this function
-// this function takes in x,y,z coordinates
+// this function takes in x,y coordinates
 // to get the hand to a certain position in space
 int ARM_LOGIC(int *In_Coords, int *Out_Pivots){
     BASE_ROTATION(In_Coords, Pivots);
@@ -75,10 +75,11 @@ void BASE_ROTATION(int *In_Coords, int *Pivots){
 // d = sqrt(x^2 + y^2)
 // now with d, we can get the angles using the law of cosines
 // we can change the arm distance if for example the pivot 1 is at 90 degrees
-// and the pivot 2 is at 360 degrees, the arm is as close as possible to the base 
+// and the pivot 2 is at 0(or 360) degrees, the arm is as close as possible to the base 
 // (technically it would be folded on itself)
 
-// if pivot 1 is at 180 degrees and pivot 2 is at 180 degrees, the arm is fully extended
+// if pivot 1 is at 90 degrees and pivot 2 is at 180 degrees, the arm is fully extended upwards
+// the furdest it can reach is when pivot 2 is at 45 and pivot 1 is at ____ 
 // so if we have the distance d,
 // we can compute r = sqrt(d^2 + z^2)
 // check if r is reachable
@@ -128,13 +129,13 @@ int ARM_ROTATIONS(int *In_Coords, int *Pivots) {
 // controlls the joint of the hand, primarly keeps it faced down releative
 // to pivot point 1 and 2
 
-// imagin angle of pivot 1 (in that case its 125 degrees)
-// and angle of pivot 2  (in that case its 135 degrees)
+// imagin angle of pivot 1 (in that case its 45 degrees)
+// and angle of pivot 2  (in that case its 100 degrees)
 // - to get the hand to be down (90 degrees relative to horizontal)
-//   you need to do 180 - (pivot1 + pivot2) = 90
+//   you need to do __________
 void WRIST_ANGLE(int *Pivots){
     // Wrist angle = always keep hand down relative to arm
-    Pivots[3] = 180 - (Pivots[1] + Pivots[2]);
+    
 }
 
 // ----- HAND CONTROL (PIV 4) ----- //
@@ -144,17 +145,8 @@ void HAND_CONTROL(int *Pivots, int Hand_action){
 }
 // ----- TRANSLATE PIVOTS ----- //
 // translates the degres into teh correct value between (0 to 205)
-//0 being _____ and 205 being _____   (motors are PWM)
-// and does the gear ratio multiplication (x5)
 void PIV_TRANSLATE(int *Pivots, int *Out_Pivots){
-    // Pivot1
-    Out_Pivots[1] = (int)((float)(Pivots[1] - 45) / (200 - 45) * 205) * 5;
-    // Pivot2
-    Out_Pivots[2] = (int)((float)(Pivots[2] - 45) / (300 - 45) * 205) * 5;
-    // Pivot3
-    Out_Pivots[3] = (int)((float)(Pivots[3] - 45) / (300 - 45) * 205) * 5;
-    // Pivot4
-    Out_Pivots[4] = (int)((float)(Pivots[4] - 0) / 125 * 205) * 5;
+
 }
 
 // ----- VERIFY PIVOTS ----- //
