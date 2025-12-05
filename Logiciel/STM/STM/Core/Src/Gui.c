4d5a636b5b367fea14556ec14fcc16a113a7e841
Thomas Cazzolato <2444121@cegeplimoilou.ca>
2025-12-05 15:48:55 -0500

    p p p p p p p

--- Logiciel/STM/STM/Core/Src/Gui.c
+++ Logiciel/STM/STM/Core/Src/Gui.c
@@ -106,20 +106,20 @@ void Run_GUI(int x_coord, int y_coord, int ctrl_mode, int *Out_Pivots, int Weigh
     static int Ancient_Mode = AUTO;
     static uint32_t timer_1 = 0;
     static int Grams = 0;
-    static AAA = 0;
+    //static AAA = 0;
 
     if (Ancient_Mode != ctrl_mode){
         LCD_Clear();
         Ancient_Mode = ctrl_mode;
         timer_1 = 0;
     }
-
+/*
         if (HAL_GetTick() - timer_1 >= 1000) {
           // Une seconde est passée
           LCD_Clear();
           AAA = ~AAA;
           timer_1 = HAL_GetTick();   // reset pour la prochaine seconde
-        }
+        }*/
     // ----- MENU 1 (AUTO) ----- //
     if (ctrl_mode == AUTO) {
        
@@ -145,8 +145,8 @@ void Run_GUI(int x_coord, int y_coord, int ctrl_mode, int *Out_Pivots, int Weigh
 
         LCD_Set(14, 0);
         LCD_Write(6); 
-        if (AAA) LCD_Write(5);
-        else LCD_Write(7);
+      //  if (AAA) LCD_Write(5);
+      //  else LCD_Write(7);
         LCD_Print("=");
         LCD_PrintInt(Out_Pivots[4]); 
         LCD_Set(9, 2);
@@ -176,40 +176,40 @@ void Run_GUI(int x_coord, int y_coord, int ctrl_mode, int *Out_Pivots, int Weigh
 
         LCD_Set(6, 0); 
         LCD_Print("-1");
-        if (AAA) LCD_Write(1);
-        else LCD_Write(3);
+     //   if (AAA) LCD_Write(1);
+     //   else LCD_Write(3);
         LCD_Print(" -4");
-        if (AAA) LCD_Write(2);
-        else LCD_Write(4);
+     //   if (AAA) LCD_Write(2);
+      //  else LCD_Write(4);
 
         LCD_Set(6, 1); 
         LCD_Print("-2");
-        if (AAA) LCD_Write(1);
-        else LCD_Write(3);
+     //   if (AAA) LCD_Write(1);
+     //   else LCD_Write(3);
         LCD_Print(" -5");
-        if (AAA) LCD_Write(2);
-        else LCD_Write(4);
+     //   if (AAA) LCD_Write(2);
+      //  else LCD_Write(4);
 
         LCD_Set(6, 2); 
         LCD_Print("-3");
-        if (AAA) LCD_Write(1);
-        else LCD_Write(3);
+       // if (AAA) LCD_Write(1);
+       // else LCD_Write(3);
         LCD_Print(" -6");
-        if (AAA) LCD_Write(2);
-        else LCD_Write(4);
+      //  if (AAA) LCD_Write(2);
+      ///  else LCD_Write(4);
 
         LCD_Set(6, 3); 
         LCD_Print("-A");
-        if (AAA) LCD_Write(1);
-        else LCD_Write(3);
+      //  if (AAA) LCD_Write(1);
+      //  else LCD_Write(3);
         LCD_Print(" -B");
-        if (AAA) LCD_Write(2);
-        else LCD_Write(4);
+      //  if (AAA) LCD_Write(2);
+      //  else LCD_Write(4);
 
         LCD_Set(14, 0);
         LCD_Write(6); 
-        if (AAA) LCD_Write(5);
-        else LCD_Write(7);
+      //  if (AAA) LCD_Write(5);
+      //  else LCD_Write(7);
         LCD_Print("=");
         LCD_PrintInt(Out_Pivots[4]); 
 
@@ -224,13 +224,13 @@ void Run_GUI(int x_coord, int y_coord, int ctrl_mode, int *Out_Pivots, int Weigh
           Grams = 20;
         }
         // weight 50G
-        else if (adc_weight >= 1000 && adc_weight <= 2000) {
-          Grams = 50;
-        }
+      //  else if (adc_weight >= 1000 && adc_weight <= 2000) {
+     //     Grams = 50;
+     //   }
         // weight 80G
-        else if (adc_weight >= 2000 && adc_weight <= 3000) {
-          Grams = 80; 
-        }
+      //  else if (adc_weight >= 2000 && adc_weight <= 3000) {
+      //    Grams = 80;
+     //   }
         // no weight
         else {
           Grams = 0;
